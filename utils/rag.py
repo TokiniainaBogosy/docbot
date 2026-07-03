63b3e7c10edf90e213231ee4ff8b81d375a94c15
tokiniainaBogosy <tokiniainaratefi@gmail.com>
2026-07-03 21:30:18 +0300

    feat: structure du projet et modules backend

--- utils/rag.py
+++ utils/rag.py
@@ -1,5 +1,29 @@
-from indexer import creer_index , rechercher
-from pdf import lire_pdf , decouper_en_chunk
+from groq import Groq
+from dotenv import load_dotenv
+import os
+from config import LLM_MODEL
+from utils.indexer import rechercher
 
-def generer_reponse(generer,index,chunks):
-    
+load_dotenv()
+client_groq = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+def generer_reponse(question, index, chunks):
+    chunks_pertinents = rechercher(question, index, chunks)
+    contexte = "\n".join([f"- {chunk}" for chunk in chunks_pertinents])
+    reponse = client_groq.chat.completions.create(
+        model=LLM_MODEL,
+        messages=[
+            {
+                "role": "system",
+                "content": """Tu es un assistant général.
+                    Réponds UNIQUEMENT en te basant sur les extraits fournis.
+                    Si la réponse n'est pas dans le contexte, dis-le clairement.
+                    Réponds en français, de manière concise."""
+            },
+            {
+                "role": "user",
+                "content": f"Contexte:\n{contexte}\n\nQuestion: {question}"
+            }
+        ]
+    )
+    return reponse.choices[0].message.content.strip() , chunks_pertinents
